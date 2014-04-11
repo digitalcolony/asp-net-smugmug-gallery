0482aee35c412928ffdcc564fd9589d9e2835b52
Michael Allen Smith <digitalcolony@gmail.com>
2014-04-11 12:00:33 -0700

    Create Gallery.aspx.cs

---
+++ Gallery.aspx.cs
@@ -0,0 +1,20 @@
+protected void Page_Load(object sender, EventArgs e)
+{
+   string rssURL;
+   string galleryID;
+   galleryID = ddlGallery.SelectedValue.ToString();
+   rssURL = "http://www.smugmug.com/hack/feed.mg?Type=gallery&Data=" + galleryID + "&format=rss200";
+
+   try
+
+   {
+       xmlDS.DataFile = rssURL;
+       dlPhotos.DataSource = xmlDS;
+       dlPhotos.DataBind();
+   }
+   catch (XmlException err)
+   {
+       lblError.Text = "Oops, looks like an error occured with this gallery: " + err.Message;
+       lblError.Visible = true;
+   }
+}
